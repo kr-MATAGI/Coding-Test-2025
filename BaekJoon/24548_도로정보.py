ae9afff86862439f5a8b9e7331fbf512e0831db9
kr-MATAGI <dev_matagi@kakao.com>
2025-02-04 16:36:59 +0900

    [Modify] BaekJoon - 24548_도로정보  - ChatGPT-o3-mini 풀이

--- BaekJoon/24548_도로정보.py
+++ BaekJoon/24548_도로정보.py
@@ -35,37 +35,38 @@
 import sys
 import copy
 from typing import List, Any, Dict
+from collections import defaultdict
 
 # 입력
 N = int(sys.stdin.readline().strip())
 road_infos = list(str(sys.stdin.readline().strip()))
 
-DP = [ [ [ [0 for _ in range(3)] for _ in range(3) ] for _ in range(3) ] for _ in range(3) ]
+state = (0, 0, 0, 0) # T, G, F, P
+
+freq = defaultdict(int)
+freq[state] = 1 # (0,0,0,0) 초기 상태 1
 
 answer = 0
-t_cnt = 0
-g_cnt = 0
-f_cnt = 0
-p_cnt = 0
-
-DP[0][0][0][0] = 1
 for item in road_infos:
-    if 'T' == item:
-        t_cnt = (t_cnt + 1) % 3
-    elif 'G' == item:
-        g_cnt = (g_cnt + 1) % 3
-    elif 'F' == item:
-        f_cnt = (f_cnt + 1) % 3
-    elif 'P' == item:
-        p_cnt = (p_cnt + 1) % 3
+    t, g, f, p = state
     
-    answer += DP[t_cnt][g_cnt][f_cnt][p_cnt]
-    DP[t_cnt][g_cnt][f_cnt][p_cnt] += 1
+    if item == 'T':
+        t = (t+1) % 3
+    elif item == 'G':
+        g = (g+1) % 3
+    elif item == 'F':
+        f = (f+1) % 3
+    elif item == 'P':
+        p = (p+1) % 3
 
-# print(f"""
-# T: {t_cnt}\n
-# G: {g_cnt}\n
-# F: {f_cnt}\n
-# P: {p_cnt}\n
-# """)
+    state = (t,g,f,p)
+
+    # 이미 같은 상태가 등장했다면, 그 횟수만큼 구간을 만들 수 있음
+    answer += freq[state]
+    
+    # 현재 상태의 등장횟수 증가
+    freq[state] += 1
+
 print(answer)
+print(freq)
+
