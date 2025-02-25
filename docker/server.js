814f803f6c0d80c5566a7d609f0db8ec827f30cf
kr-MATAGI <dev_matagi@kakao.com>
2025-02-25 13:13:43 +0900

    [Add] Orchestration

--- docker/server.js
+++ docker/server.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
 
-app.listen(8080, () => {
+const server = app.listen(8080, () => {
   console.log('서버 실행중 http://localhost:8080');
 });
 
 app.get('/', (req, res) => {
-  res.send('안녕');
-}); 
+  res.send('안녕2211dddxxx111x !!!!!!');
+});
+
+process.on('SIGTERM', () => {
+  server.close(() => {
+    console.log('HTTP server closed')
+  })
+})
+process.on('SIGINT', () => {
+  server.close(() => {
+    console.log('HTTP server closed')
+  })
+}) 
