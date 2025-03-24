e684f49ab17021fd2daec55c79d48a91e8f5302a
kr-MATAGI <dev_matagi@kakao.com>
2025-03-24 19:49:55 +0900

    [Add] 프로그래머스 - 2020 카카오 인턴십 보석쇼핑

---
+++ Programmers/pg_2020_카카오인턴십_보석쇼핑.py
@@ -0,0 +1,18 @@
+'''
+https://school.programmers.co.kr/learn/courses/30/lessons/67258
+
+
+
+'''
+
+
+def solution(gems):
+    answer = []
+
+    
+    return answer
+
+### MAIN ###
+if "__main__" == __name__:
+    ans_1 = solution(["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"])
+    print(ans_1)
