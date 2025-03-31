bcc23e84104c702e65498a4c025b0ee2718e06be
kr-MATAGI <dev_matagi@kakao.com>
2025-03-31 18:58:04 +0900

    [Fix] 슬라이딩 윈도우 계속 갱신하는 법 추가

--- Programmers/pg_2020_카카오인턴십_보석쇼핑.py
+++ Programmers/pg_2020_카카오인턴십_보석쇼핑.py
@@ -6,49 +6,44 @@ https://school.programmers.co.kr/learn/courses/30/lessons/67258
 진열된 모들 종류의 보석을 모두 적어도 1개 이상 포함하는 가장 짧은 구간을 찾아서 구매
 
 '''
+from collections import defaultdict
 
 def solution(gems):
     '''
         1) 고유한 값들 보관
-        2) s와 e를 통해 자릿수 인덱싱
-        3) e를 앞세워서 고유한 값들 최소 1개씩 포함될때까지 앞으로 이동
-        4) 다 찾으면 s를 하나씩 줄임
+        2) left와 right를 이용해서 계산
+        3) right를 하나씩 옮김 -> 다 찾았는가 -> left 옮김
+        4) left 옮겼을 때 최소 거리 갱신 -> 0으로 되면 해당 삭제 -> 다시 rigth 옮길 수 있도록
     '''
     answer = []
+    min_dist = len(gems)
 
     # 고유한 값들 확인
     target_set = set(gems)
 
     # 현재 보석 보유량 확인할 dict
-    gem_cnt_info = {x: 0 for x in target_set}
-    # print(cur_gem_info)
+    gem_cnt_info = defaultdict(int)
+    gem_cnt_info[gems[0]] = 1
 
     # 계산
-    s, e = 0, 0 # start_idx, end_idx
-
-    cur_set = set() # 현재 중복제거한 보석들
-    while True:
-        cur_gem = gems[e]
-        gem_cnt_info[cur_gem] += 1
-        cur_set.add(cur_gem)
-        if cur_set == target_set:
-            # 모든 보석이 1개 이상씩 존재
-            break
-
-        e += 1
-
-    # s를 1씩 높여가봄
-    while True:
-        cur_gem = gems[s]
-        # print(f"s: {s}, cur_gem: {cur_gem}, gem_cnt_info: {gem_cnt_info[cur_gem]}")
-        if 1 <= gem_cnt_info[cur_gem] - 1:
-            # 해당 보석이 아직 1개 이상 있음
-            gem_cnt_info[cur_gem] -= 1
-            s += 1
+    left, right = 0, 0
+    while left <= right < len(gems):
+        if len(gem_cnt_info) != len(target_set):
+            # 아직 모든 종류를 포함하지 않음
+            right += 1
+            if right >= len(gems):
+                break
+            gem_cnt_info[gems[right]] += 1
         else:
-            break
+            # 모든 종류가 포함되었으면 왼쪽 줄이기
+            if right - left < min_dist:
+                answer = [left + 1, right + 1]
+                min_dist = right - left
+            gem_cnt_info[gems[left]] -= 1
+            if gem_cnt_info[gems[left]] == 0:
+                del gem_cnt_info[gems[left]]
+            left += 1
 
-    answer = [s + 1, e + 1]
     return answer
 
 
@@ -67,3 +62,6 @@ if "__main__" == __name__:
 
     ans_5 = solution(["A", "B", "C", "A", "A", "B", "C", "C"])
     print(f"ans_5: {ans_5}\n")
+
+    ans_6 = solution(["A", "A", "B", "B", "B", "C", "C", "A", "A", "B", "C"])
+    print(f"ans_6: {ans_6}\n")
